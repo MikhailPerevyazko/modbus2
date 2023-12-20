76e37c2f912f3d02e01b22ae9412a9cd3b44f3f1
Mikhail Perevyazko <perevyazko.misha2001@gmail.com>
2023-12-20 15:46:22 +0300

    some commit

--- modbus_app/src/cmd.rs
+++ modbus_app/src/cmd.rs
@@ -4,26 +4,19 @@ use std::path::PathBuf;
 
 pub fn get_path() {
     let args = Args::parse();
-    let home_dir = std::env::var("HOME").unwrap();
-    let file_path = match args.file_path {
+    let home_dir: String = std::env::var("HOME").unwrap();
+    let file_path: PathBuf = match args.file_path {
         Some(path) => path,
         None => PathBuf::from(home_dir)
             .join(".config")
             .join("simple_modbusclient")
             .join("config.yaml"),
-    };
+    };   
+}
 
-    pub struct PathState(PathBuf);
-    impl PathState {
-        pub fn path(&self) -> PathBuf {
-            self.0.to_owned()
-        }
-    }
-    
-    #[derive(Parser, Debug)]
-    #[command(author, version, about, long_about = None)]
-    pub struct Args {
-        #[arg(short, long)]
-        file_path: Option<PathBuf>, 
-    }
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long)]
+    file_path: Option<PathBuf>, 
 }
